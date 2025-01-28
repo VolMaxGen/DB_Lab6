e8aecfed30221d093bdfd275e88b24327c690fd9
VolMax <volmaxdev2@gmail.com>
2025-01-28 13:38:03 +0300

    Добавьте файлы проекта.

---
+++ lab6/Migrations/20250108202813_InitialCreate.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lab6.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dishes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    Cost = table.Column<double>(type: "float", nullable: false),
+                    CookingTime = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dishes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingridients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingridients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Professions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Providers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Providers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngridientsDishes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DishId = table.Column<int>(type: "int", nullable: false),
+                    IngridientId = table.Column<int>(type: "int", nullable: false),
+                    IngridientWeight = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngridientsDishes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IngridientsDishes_To_Dishes",
+                        column: x => x.DishId,
+                        principalTable: "Dishes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngridientsDishes_To_Ingridients",
+                        column: x => x.IngridientId,
+                        principalTable: "Ingridients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    MiddleName = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: true, defaultValueSql: "((18))"),
+                    Education = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    ProfessionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_To_Professions",
+                        column: x => x.ProfessionId,
+                        principalTable: "Professions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngridientsWarehouses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IngridientId = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: true, defaultValueSql: "((0))"),
+                    ReleaseDate = table.Column<DateTime>(type: "date", nullable: false),
+                    StorageLife = table.Column<DateTime>(type: "date", nullable: false),
+                    Cost = table.Column<double>(type: "float", nullable: false),
+                    ProviderId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngridientsWarehouses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IngridientsWarehouses_To_Ingridients",
+                        column: x => x.IngridientId,
+                        principalTable: "Ingridients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngridientsWarehouses_To_Providers",
+                        column: x => x.ProviderId,
+                        principalTable: "Providers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderDate = table.Column<DateTime>(type: "smalldatetime", nullable: false),
+                    CustomerName = table.Column<string>(type: "varchar(30)", unicode: false, maxLength: 30, nullable: true),
+                    CustomerPhoneNumber = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    PaymentMethod = table.Column<int>(type: "int", nullable: false),
+                    IsCompleted = table.Column<int>(type: "int", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_To_Employees",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDishes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    DishId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDishes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderDishes_To_Dishes",
+                        column: x => x.DishId,
+                        principalTable: "Dishes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDishes_To_Orders",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Dishes__737584F6513A3CE2",
+                table: "Dishes",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_ProfessionId",
+                table: "Employees",
+                column: "ProfessionId");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Ingridie__737584F6A66E861B",
+                table: "Ingridients",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngridientsDishes_DishId",
+                table: "IngridientsDishes",
+                column: "DishId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngridientsDishes_IngridientId",
+                table: "IngridientsDishes",
+                column: "IngridientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngridientsWarehouses_IngridientId",
+                table: "IngridientsWarehouses",
+                column: "IngridientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngridientsWarehouses_ProviderId",
+                table: "IngridientsWarehouses",
+                column: "ProviderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDishes_DishId",
+                table: "OrderDishes",
+                column: "DishId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDishes_OrderId",
+                table: "OrderDishes",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_EmployeeId",
+                table: "Orders",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Professi__737584F6D7EB01B2",
+                table: "Professions",
+                column: "Name",
+                unique: true,
+                filter: "([Name] IS NOT NULL)");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Provider__737584F62645BCF3",
+                table: "Providers",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngridientsDishes");
+
+            migrationBuilder.DropTable(
+                name: "IngridientsWarehouses");
+
+            migrationBuilder.DropTable(
+                name: "OrderDishes");
+
+            migrationBuilder.DropTable(
+                name: "Ingridients");
+
+            migrationBuilder.DropTable(
+                name: "Providers");
+
+            migrationBuilder.DropTable(
+                name: "Dishes");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Professions");
+        }
+    }
+}
